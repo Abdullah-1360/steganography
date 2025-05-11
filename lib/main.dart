fad800cc4d402d813159c04196a430bee26af5bc
Abdullah-1360 <abdullah.shah78963@gmail.com>
2025-05-11 19:33:05 +0500

    final working

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:steganography/stegoservice.dart'; // Import your service file
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
